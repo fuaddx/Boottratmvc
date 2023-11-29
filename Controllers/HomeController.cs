bca8b664885d16aaa22a0651d6db8fbda0537ba7
fuaddx <mi848xfcl@code.edu.az>
2023-11-29 13:15:24 +0400

    mvcc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ClassWork1.Helpers;
+using ClassWork1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClassWork1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public async Task<IActionResult> Index()
+        {
+            var dt = await SqlHelper.GetDatas("SELECT * FROM TableData");
+            return View(dt);
+        }
+
+
+       
+    }
+}
---
+++ Helpers/SqlHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassWork1.Helpers
+{
+    public class SqlHelper
+    {
+        private const string _connectionString = @"Server=DESKTOP-GTD240T\SQLEXPRESS;Database=DataTable;Trusted_Connection=true";
+        public static async Task<DataTable> GetDatas(string query)
+        {
+            using SqlConnection connection = new SqlConnection(_connectionString);
+             await  connection.OpenAsync();
+            DataTable dt = new DataTable();
+            using SqlDataAdapter sda = new SqlDataAdapter(query, connection);
+            sda.Fill(dt);
+           await connection.CloseAsync();
+            return dt;
+        }
+        public static async Task<int> Exec(string query)
+        {
+            using SqlConnection connection = new SqlConnection(_connectionString);
+           await  connection.OpenAsync();
+            using SqlCommand command = new SqlCommand(query, connection);
+            int result = await command.ExecuteNonQueryAsync();
+           await connection.CloseAsync();
+            return result;
+        }
+    }
+}
